e8938ba28d6e6e75473e50356afc884f39c37dd1
Ahmad Trefi <ahmadtrefi@MacBookAir.fritz.box>
2026-06-04 18:08:47 +0200

    Add console demo application

--- student-management-system/src/main/java/de/hs/student/App.java
+++ student-management-system/src/main/java/de/hs/student/App.java
@@ -1,13 +1,23 @@
 package de.hs.student;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+
+    public static void main(String[] args) {
+        StudentRepository repository = new StudentRepository();
+        StudentService studentService = new StudentService(repository);
+        StudentReportService reportService = new StudentReportService(repository);
+
+        Student student = new Student(
+                "1",
+                "Ahmad",
+                "Trefi",
+                "trefiahm@hs-albsig.de"
+        );
+
+        studentService.addStudent(student);
+        studentService.addGradeToStudent("1", new Grade("Java Development", 1.7));
+        studentService.addGradeToStudent("1", new Grade("Software Engineering", 2.3));
+
+        System.out.println(reportService.generateReport("1"));
     }
 }
